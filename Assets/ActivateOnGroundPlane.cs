25ceb6dc86d722b03559a264abcc011c25f2e8a6
jim-martin <jim.martin@mapbox.com>
2018-01-26 23:24:07 -0800

    mapbox + ar resources

---
+++ Assets/ActivateOnGroundPlane.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityARInterface;
+
+public class ActivateOnGroundPlane : MonoBehaviour {
+
+	void OnEnable()
+	{
+		ARInterface.planeAdded += PlaneAddedHandler;
+		//ARInterface.planeUpdated += PlaneUpdatedHandler;
+		//ARInterface.planeRemoved += PlaneRemovedHandler;
+	}
+
+	void OnDisable()
+	{
+		//ARInterface.planeAdded -= PlaneAddedHandler;
+		//ARInterface.planeUpdated -= PlaneUpdatedHandler;
+		//ARInterface.planeRemoved -= PlaneRemovedHandler;
+	}
+
+	void PlaneAddedHandler(BoundedPlane plane)
+	{
+		var newPosition = transform.localPosition;
+		newPosition.y = plane.center.y;
+
+		transform.position = newPosition;
+		ARInterface.planeAdded -= PlaneAddedHandler;
+	}
+}
---
+++ Assets/SetRenderQueue.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("Rendering/SetRenderQueue")]
+
+public class SetRenderQueue : MonoBehaviour
+{
+
+	[SerializeField]
+	protected int[] m_queues = new int[] { 3000 };
+
+	protected void Start()
+	{
+		var renderers = GetComponentsInChildren<MeshRenderer>();
+		foreach( var r in renderers)
+		{
+			Material[] materials = r.materials;
+			for (int i = 0; i < materials.Length && i < m_queues.Length; ++i)
+			{
+				materials[i].renderQueue = m_queues[i];
+			}
+
+		}
+	}
+}
